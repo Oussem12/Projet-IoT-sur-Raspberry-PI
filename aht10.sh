0dc0fdd986fda349d2a8f7722df4cf6695be17b7
oussema <oussamabenhmida24@gmail.com>
2024-08-26 19:56:06 +0100

    modification de la script shell

--- aht10.sh
+++ aht10.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-AHT10_ADDR="0x38"
-AHT10_DRIVER="aht10"
-I2C_NEW_DEVICE="/sys/bus/i2c/devices/i2c-1/new_device"
-
-for driver in i2c-bcm2708 i2c-dev aht10; do 
-    if ! lsmod | grep -q $driver; then
-        modprobe $driver || echo "Could not load $driver"
-    fi
-done
-
-if echo ${AHT10_DRIVER} ${AHTH10_ADDR} > ${I2C_NEW_DEVICE}; then
-    python3 aht10.py
-else
-    echo "Could not add new_device for ${AHT10_DRIVER}"
+if ! lsmod | grep -q i2c_dev; then
+    modprobe i2c-dev
 fi
+
+if ! lsmod | grep -q i2c_bcm2708; then
+    modprobe i2c-bcm2708
+fi
+  modprobe aht10 lsmod | grep i2c lsmod | grep aht10 echo aht10 0x38 > /sys/bus/i2c/devices/i2c-1/new_device   
+i2cdetect -l
+i2cdetect -y 1
+#!/bin/bash
+
+if ! lsmod | grep -q i2c_dev; then
+    modprobe i2c-dev
+fi
+
+if ! lsmod | grep -q i2c_bcm2708; then
+    modprobe i2c-bcm2708
+fi
+  modprobe aht10 lsmod | grep i2c lsmod | grep aht10 echo aht10 0x38 > /sys/bus/i2c/devices/i2c-1/new_device   
+i2cdetect -l
+i2cdetect -y 1
+  python3 aht10.py
+
