20779faf493c577c6e70a705d780ba7a85e5803e
oussema <oussamabenhmida24@gmail.com>
2024-08-26 20:00:11 +0100

    script shell pour initialiser le capteur

--- aht10.sh
+++ aht10.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Charger les modules nécessaires si non déjà chargés
 if ! lsmod | grep -q i2c_dev; then
     modprobe i2c-dev
 fi
@@ -7,20 +8,18 @@ fi
 if ! lsmod | grep -q i2c_bcm2708; then
     modprobe i2c-bcm2708
 fi
-  modprobe aht10 lsmod | grep i2c lsmod | grep aht10 echo aht10 0x38 > /sys/bus/i2c/devices/i2c-1/new_device   
+
+# Charger le module aht10
+modprobe aht10
+
+# Ajouter le périphérique I2C
+echo "aht10 0x38" > /sys/bus/i2c/devices/i2c-1/new_device
+
+# Vérifier la détection des périphériques I2C
 i2cdetect -l
 i2cdetect -y 1
-#!/bin/bash
 
-if ! lsmod | grep -q i2c_dev; then
-    modprobe i2c-dev
-fi
+# Exécuter le script Python
+python3 aht10.py
 
-if ! lsmod | grep -q i2c_bcm2708; then
-    modprobe i2c-bcm2708
-fi
-  modprobe aht10 lsmod | grep i2c lsmod | grep aht10 echo aht10 0x38 > /sys/bus/i2c/devices/i2c-1/new_device   
-i2cdetect -l
-i2cdetect -y 1
-  python3 aht10.py
 
